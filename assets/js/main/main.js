72436dbc764c4b34a12cb940917ef99e52ff209d
tuongduy10 <vtngduy101120@gmail.com>
2022-01-10 23:46:36 +0700

    first commit

---
+++ assets/js/main/main.js
@@ -0,0 +1,14 @@
+document.getElementById('nav__menubar-open').onclick = function(){
+    document.getElementById('nav__overlay').className = document.getElementById('nav__overlay').className.replace('d-none', 'd-block')
+    document.getElementById('nav__mobile-list-open').className = document.getElementById('nav__mobile-list-open').className.replace('d-none', 'd-block')
+}
+document.getElementById('nav__overlay').onclick = function(){
+    closeNavMobileList()
+}
+document.getElementById('nav__mobile-list-close').onclick = function(){
+    closeNavMobileList()
+}
+function closeNavMobileList(){
+    document.getElementById('nav__overlay').className = document.getElementById('nav__overlay').className.replace('d-block', 'd-none')
+    document.getElementById('nav__mobile-list-open').className = document.getElementById('nav__mobile-list-open').className.replace('d-block', 'd-none')
+}
