c015c21f06a4fde5c73e991e1c5f01b701a10265
tuongduy10 <vtngduy101120@gmail.com>
2022-01-19 11:47:12 +0700

    add slick slider, svg icon

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -1,14 +1,14 @@
-document.getElementById('nav__menubar-open').onclick = function(){
+document.getElementById('nav__menubar-open').onclick = function () {
     document.getElementById('nav__overlay').className = document.getElementById('nav__overlay').className.replace('d-none', 'd-block')
     document.getElementById('nav__mobile-list-open').className = document.getElementById('nav__mobile-list-open').className.replace('nav-close', 'nav-open')
 }
-document.getElementById('nav__overlay').onclick = function(){
+document.getElementById('nav__overlay').onclick = function () {
     closeNavMobileList()
 }
-document.getElementById('nav__mobile-list-close').onclick = function(){
+document.getElementById('nav__mobile-list-close').onclick = function () {
     closeNavMobileList()
 }
-function closeNavMobileList(){
+function closeNavMobileList() {
     document.getElementById('nav__overlay').className = document.getElementById('nav__overlay').className.replace('d-block', 'd-none')
     document.getElementById('nav__mobile-list-open').className = document.getElementById('nav__mobile-list-open').className.replace('nav-open', 'nav-close')
 }
