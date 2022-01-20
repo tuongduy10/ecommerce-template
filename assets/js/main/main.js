8f02926026a0bdb9f35f052c8f44ba2f47ae15dc
tuongduy10 <vtngduy101120@gmail.com>
2022-01-20 23:22:25 +0700

    Fixed web directional

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -12,3 +12,18 @@ function closeNavMobileList() {
     document.getElementById('nav__overlay').className = document.getElementById('nav__overlay').className.replace('d-block', 'd-none')
     document.getElementById('nav__mobile-list-open').className = document.getElementById('nav__mobile-list-open').className.replace('nav-open', 'nav-close')
 }
+
+
+$(".order__item-wrapper").hover(
+    function () {
+        $(".order__item-dropdown_menu").addClass("open");
+        $(".svg-order").addClass("svg-up");
+    }, function () {
+        $(".order__item-dropdown_menu").removeClass("open");
+        $(".svg-order").removeClass("svg-up");
+    }
+);
+$(".order__item-wrapper").focus(function () {
+    $(".order__item-dropdown_menu").addClass("open");
+    $(".svg-order").addClass("svg-up");
+})
