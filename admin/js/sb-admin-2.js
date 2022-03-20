5532392e4f8f36e15f4858dc0d0d125f3d3b0a80
tuongduy10 <vtngduy101120@gmail.com>
2022-03-20 15:50:36 +0700

    fixing editor

--- admin/js/sb-admin-2.js
+++ admin/js/sb-admin-2.js
@@ -11,18 +11,18 @@
   });
 
   // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
-    if ($(window).width() < 768) {
-      $('.sidebar .collapse').collapse('hide');
-    };
+  // $(window).resize(function() {
+  //   if ($(window).width() < 768) {
+  //     $('.sidebar .collapse').collapse('hide');
+  //   };
     
-    // Toggle the side navigation when window is resized below 480px
-    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
-      $("body").addClass("sidebar-toggled");
-      $(".sidebar").addClass("toggled");
-      $('.sidebar .collapse').collapse('hide');
-    };
-  });
+  //   // Toggle the side navigation when window is resized below 480px
+  //   if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+  //     $("body").addClass("sidebar-toggled");
+  //     $(".sidebar").addClass("toggled");
+  //     $('.sidebar .collapse').collapse('hide');
+  //   };
+  // });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
